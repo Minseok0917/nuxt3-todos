82b6979708213d1f090fbb47a05d639ccd9fbcc1
Minseok Jung <37059223+Minseok0917@users.noreply.github.com>
2023-09-04 17:55:21 +0900

    config : tailwind 재설정

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["nuxt-icon", "@nuxtjs/eslint-module", "@nuxtjs/tailwindcss"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  css: ["~/assets/scss/module.scss"],
+  modules: ["nuxt-icon", "@nuxtjs/eslint-module"],
 });
